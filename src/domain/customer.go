e2da927397140526eb7a97ebbea366af050b80a0
LimbergDV <limbergdelvalle@gmail.com>
2025-03-09 18:29:03 -0600

    add: adding domain in src

--- src/domain/customer.go
+++ src/domain/customer.go
@@ -1,1 +1,20 @@
 package domain
+
+type Customer struct {
+	Id int
+	Name string
+	Last_name string
+	Phone_number string
+	Curp string
+	Number_license string
+}
+
+type Links struct {
+	Self string
+}
+
+type Response struct {
+	Data   []Customer
+	Links  Links
+	Available bool
+}
--- src/domain/notify.go
+++ src/domain/notify.go
@@ -1,1 +1,6 @@
 package domain
+
+type Notify struct {
+	Id_customer int
+	Return_date string
+}
